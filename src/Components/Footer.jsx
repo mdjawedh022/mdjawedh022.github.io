46392dca5b355518b577fdf1e25d3c890383fd17
Md Jawed <mdjawedh22@gmail.com>
2023-05-02 17:34:17 +0530

    portfolio

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,23 @@
+import { Box, useColorModeValue,Text, Flex } from '@chakra-ui/react'
+import React from 'react'
+import { MdEmail, MdPhone } from 'react-icons/md'
+
+const Footer = () => {
+  return (
+    <>
+      <Box w={{base:'100%',md:"100%",lg:"100%"}} color={'white'}   bg={useColorModeValue("gray.800","blue.800")} mt={'10px'} >
+        <br />
+        <Text color={'white'} p={'30px auto'}>Designed and build by Md Jawed Hussain, 2023 All rights reserved.</Text>
+      
+      <Box w={{lg:"20%",base:"64%" ,md:"40%"}}m='auto' textAlign={"center"} pl={{base:"5px",md:"40px"}}>
+      {/* <Text textAlign={'center'} display={"flex"}alignItems={"center"}  id="contact-email" label="jawedhilmand022@gmail.com"><MdEmail/>: jawedhilmand022@gmail.com</Text> */}
+        <Text display={"flex"}alignItems={"center"} id="contact-phone" label="8271766683"><MdPhone />: +91 8271766683</Text>
+      </Box>
+       
+        <br />
+      </Box>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,108 @@
+import { Box, Button, Text, Image, useColorModeValue } from "@chakra-ui/react";
+import React from "react";
+import { Typewriter } from "react-simple-typewriter";
+import "./home.css";
+import { DownloadIcon } from "@chakra-ui/icons";
+import img from "../../Data/jawed-1.jpg";
+import resume from "../../Data/Md-Jawed-Hussain_Resume.pdf";
+
+const Home = () => {
+  const handleclick = () => {
+    window.open(
+      "https://drive.google.com/file/d/1I2fdc7cMx8L-cPzQp9ThzusDJWUNjvcm/view?usp=share_link"
+    );
+  };
+  return (
+    <>
+      <Box
+        id="home"
+        name="home"
+        pt={{ base: "70px",sm:"130px", md: "70px", lg: "110px" }}
+        bg={useColorModeValue("blue.800", "gray.700")}
+        display={{ base: "grid", md: "grid", lg: "grid" }}
+        gridTemplateColumns={{
+          base: "repeat(1,1fr)",
+          md: "repeat(2,1fr)",
+          lg: "repeat(2,1fr)",
+        }}
+        w={{ base: "100%", md: "100%", lg: "100%" }}
+        // mt={{ base: "16.5%", md: "9.5%", lg: "5.8%" }}
+        alignItems={"center"}
+      >
+        <Box w={{ base: "100%", md: "100%", lg: "100%" }}>
+          <Text
+            color={useColorModeValue("white")}
+            fontSize={{ base: "30px", md: "40px", lg: "50px" }}
+          >
+            Hi, My Name is
+          </Text>
+          <Text
+            id="user-detail-name"
+            color={useColorModeValue("white")}
+            fontSize={{ base: "35px", md: "45px", lg: "60px" }}
+          >
+            Md Jawed Hussain
+          </Text>
+          <Text
+            color={useColorModeValue("white")}
+            fontSize={{ base: "14px", md: "20px", lg: "22px" }}
+          >
+            I enjoy coding and devloping user centeric web aplication!
+          </Text>
+          <Text
+            color={useColorModeValue("white")}
+            fontSize={{ base: "15px", md: "20px", lg: "20px" }}
+          >
+            {" "}
+            I am{" "}
+            <Typewriter
+              words={[" Full Stack Developer", " Full Stack Developer"]}
+              loop
+              typeSpeed={100}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </Text>
+          <Button
+            id="resume-button-2"
+            color={useColorModeValue("", "black")}
+            bg={"white"}
+            m={"10px"}
+            onClick={handleclick}
+          >
+            <a
+              href={resume}
+              download="Md-Jawed-Hussain-Resume"
+              id="resume-link-2"
+            >
+              Resume <DownloadIcon />
+            </a>
+          </Button>
+
+          {/* <a href={resume}
+ download="">
+
+</a> */}
+        </Box>
+        <Box w={{ base: "100%", md: "100%", lg: "100%" }}>
+          <Box
+            borderWidth={"5px"}
+            w={{ base: "70%", md: "60%", lg: "35%" }}
+            borderRadius={"90%"}
+            m={{ base: "15px auto",sm:"15px auto", md: "30px auto", lg: "95px auto" }}
+          >
+            <Image
+              className="home-img"
+              src={img}
+              w={"100%"}
+              cursor={"pointer"}
+              borderRadius={"90%"}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Home;
